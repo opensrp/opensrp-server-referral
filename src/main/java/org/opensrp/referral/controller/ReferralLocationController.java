ddcce4672c71b7c00753614e560faecedfd41cca
cozej4 <ilakozejumanne@gmail.com>
2020-01-27 15:23:39 +0300

    Code refactoring

--- src/main/java/org/opensrp/referral/controller/ReferralLocationController.java
+++ src/main/java/org/opensrp/referral/controller/ReferralLocationController.java
@@ -2,14 +2,18 @@ package org.opensrp.referral.controller;
 
 import com.google.gson.Gson;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.opensrp.referral.service.ReferralLocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 @RequestMapping("/location/")
@@ -26,14 +30,26 @@ public class ReferralLocationController {
     /**
      * This method receives the uuid of a facility location that team member (chw) is tied up to
      * or a uuid of a village that team member is serving and return a list of all other facilities within the district/council of the uuid
-     * @param  uuid uuid of a facility or a village.
-     * @return List of all facilities within the same district/council.
+     * @param  jsonObject object containing
+     *              locationUUID of any location within the hierarchy level within the a hierarchy,
+     *              locationTopLevel, string of the tag name of top location hierarchy,
+     *              allowedTags, a jsonArray containing TAGS in the location hierarchy,
+     *              locationTagsQueried, a jsonArray of containing locations to be returned
+     * @return List of all facilities within the same hierarchy level.
      */
 
-    @RequestMapping("facilities/{uuid}")
-    @ResponseBody
-    public ResponseEntity<String> getFacilitiesWithinACouncil(@PathVariable("uuid") final String uuid) throws JSONException {
-        return new ResponseEntity<>(new Gson().toJson(referralLocationService.getLocationsWithinAHierarchyLevel(uuid)), HttpStatus.OK);
+    @RequestMapping(headers = {"Accept=application/json"}, method = POST, value = "locations/getLocationsByTags")
+    public ResponseEntity<String> getFacilitiesWithinACouncil(@RequestBody JSONObject jsonObject) throws JSONException {
+
+
+        return new ResponseEntity<>(new Gson().toJson(
+                referralLocationService.getLocationsWithinAHierarchyLevel(
+                        jsonObject.getString("locationUUID"),
+                        jsonObject.getString("locationTopLevel"),
+                        jsonObject.getJSONArray("allowedTags"),
+                        jsonObject.getJSONArray("locationTagsQueried")
+                )
+        ), HttpStatus.OK);
     }
 
 }
