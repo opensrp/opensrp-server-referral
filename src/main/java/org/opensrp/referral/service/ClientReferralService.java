b0d790c3ba91ff599778dbbc505dace78e1aca46
Elly Kitoto <junkmailstoelly@gmail.com>
2020-02-21 15:56:14 +0300

    Add missing service annotation

--- src/main/java/org/opensrp/referral/service/ClientReferralService.java
+++ src/main/java/org/opensrp/referral/service/ClientReferralService.java
@@ -2,10 +2,12 @@ package org.opensrp.referral.service;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.stereotype.Service;
 
 /**
  * Added as placeholder for client referral work
  */
+@Service
 public class ClientReferralService {
     public JSONObject testReferralService() throws JSONException {
         return new JSONObject().put("success", "testing referral api");
